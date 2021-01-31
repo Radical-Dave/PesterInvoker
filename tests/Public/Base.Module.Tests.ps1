482b110ac8598dc387820cc57861c5b8845cd4e1
David Walker <dave@radicaldave.com>
2021-01-31 01:09:15 -0600

    updates

--- tests/Public/Base.Module.Tests.ps1
+++ tests/Public/Base.Module.Tests.ps1
@@ -7,16 +7,16 @@ InModuleScope $ModuleScriptName {
     $ModuleScriptName = Split-Path (Split-Path (Split-Path $PSScriptRoot -Parent) -Parent) -Leaf
     $ModuleScriptPath = Join-Path $PSScriptRoot "../../src/$ModuleScriptName/$ModuleScriptName.psm1"
     Describe "$ModuleScriptName.Module.Base.Tests" {
-        $ModuleScriptName = Split-Path (Split-Path (Split-Path $PSScriptRoot -Parent) -Parent) -Leaf
+        #$ModuleScriptName = Split-Path (Split-Path (Split-Path $PSScriptRoot -Parent) -Parent) -Leaf
         $ModuleScriptPath = Join-Path $PSScriptRoot "../../src/$ModuleScriptName/$ModuleScriptName"
         It 'imports successfully' {
-            $ModuleScriptName = Split-Path (Split-Path (Split-Path $PSScriptRoot -Parent) -Parent) -Leaf
+            #$ModuleScriptName = Split-Path (Split-Path (Split-Path $PSScriptRoot -Parent) -Parent) -Leaf
             $ModuleScriptPath = Join-Path $PSScriptRoot "../../src/$ModuleScriptName"
             Write-Verbose "Import-Module -Name $($ModuleScriptPath)"
             { Import-Module -Name $ModuleScriptPath -ErrorAction Stop } | Should -Not -Throw
         }
         It 'passes default PSScriptAnalyzer rules' {
-            $ModuleScriptName = Split-Path (Split-Path (Split-Path $PSScriptRoot -Parent) -Parent) -Leaf
+            #$ModuleScriptName = Split-Path (Split-Path (Split-Path $PSScriptRoot -Parent) -Parent) -Leaf
             $ModuleScriptPath = Join-Path $PSScriptRoot "../../src/$ModuleScriptName"
             Invoke-ScriptAnalyzer -Path $ModuleScriptPath | Should -BeNullOrEmpty
         }
@@ -24,7 +24,7 @@ InModuleScope $ModuleScriptName {
 
     Describe "$ModuleScriptName.Module.Manifest.Base.Tests" {
         It 'passes Test-ModuleManifest' {
-            $ModuleScriptName = Split-Path (Split-Path (Split-Path $PSScriptRoot -Parent) -Parent) -Leaf
+            #$ModuleScriptName = Split-Path (Split-Path (Split-Path $PSScriptRoot -Parent) -Parent) -Leaf
             $ModuleManifestName = "$ModuleScriptName.psd1"
             $ModuleManifestPath = Join-Path $PSScriptRoot "../../src/$ModuleScriptName/$ModuleManifestName"
             Write-Verbose $ModuleManifestPath
